122368214caac37fe15980650a10495dd7ca3593
Chironjit Das <chironjit@gmail.com>
2026-04-14 10:42:25 +1000

    Revert "Merge pull request #2 from chironjit/addtests"
    This reverts commit 9d3de51b71cb0976cdb160a02492a147189d51f7, reversing
    changes made to 4819f1bf4944438f7d59030cb0c4385a3292fb78.

--- json.go
+++ json.go
@@ -30,7 +30,5 @@ func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 		return
 	}
 	w.WriteHeader(code)
-	if _, err := w.Write(dat); err != nil {
-		log.Printf("Error writing response: %v", err)
-	}
+	w.Write(dat)
 }
